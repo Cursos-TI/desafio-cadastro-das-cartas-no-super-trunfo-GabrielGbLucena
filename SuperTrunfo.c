c5ec8d0ffc08c0fe12840389166a9b0e5e974856
GabrielGbLucena <lucenagabryel@gmail.com>
2025-04-03 20:49:25 -0300

    SuperTrunfo.c

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -1,16 +1,7 @@
 #include <stdio.h> 
 
 int main (){
-  int populaçãoa01 = 
-  int populaçãob02 = 
-  float areaa01 = 
-  float areab02 = 
-  float piba01 = 
-  float pibb02 = 
-  int numero de pontos turisticosa01 = 
-  int numero de pontos turisticosb02 = 
 
-  printf("População A01: %d habitantes\n" populaçãoa01); 
 
   
 return 0; 
